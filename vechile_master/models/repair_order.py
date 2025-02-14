4d3be654e560a622782998b60fd407de9f63144c
rakesh-fidelis <rakesh.s@fidelisgroup.in>
2025-02-14 11:34:05 +0530

    Delete vechile_master directory
    unwanted files

--- vechile_master/models/repair_order.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class RepairOrder(models.Model):
-    _inherit = 'repair.order'
-
-    repair_line_type = fields.Selection(default='add', selection=[('add', 'Add'), ('remove', 'Remove')],
-                                        string='Repair Line Type')
-    sequence = fields.Integer(string='Sequence')
